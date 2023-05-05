0904b45ca3830fd464cf60129ed208d770f9d07a
jisue <tjdwltn121@gmail.com>
2023-05-05 16:45:22 +0900

    Initial commit

---
+++ src/test/java/hello/springboardproject/SpringBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
